72761666a13e396420b951cf3335e30ed0e7b26d
Casey Megginson <tenncasey@gmail.com>
2016-05-03 00:55:09 -0500

    Finally fixed issues so it builds correctly.

--- incarceration_rates/executions.ts
+++ incarceration_rates/executions.ts
@@ -1,9 +1,10 @@
-// "use strict";
+import * as d3 from 'd3';
+"use strict";
 
 let data = d3.csv("data/cp_executed.csv");
 let i:number = 1;
 
-for (let item of data) {
+for (let item in data) {
 
     i++;
 }
