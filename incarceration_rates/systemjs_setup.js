6ce26f9a1f81fad34d94b8fb1936927edbae80eb
Casey Megginson <tenncasey@gmail.com>
2016-05-03 01:54:55 -0500

    Removed systemjs_setup.js since it's now redundant.

--- incarceration_rates/systemjs_setup.js
+++
@@ -1,8 +0,0 @@
-System.config({
-  packages: {
-    app: {format: 'register', defaultExtension: 'js'},
-    'node_modules/d3': {main: 'd3.js', format: 'cjs', defaultExtension: 'js'},
-  },
-  paths: {'d3': 'node_modules/d3'}
-});
-System.import('executions.js').then(null, console.error.bind(console));
