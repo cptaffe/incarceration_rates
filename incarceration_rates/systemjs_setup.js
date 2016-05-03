4dfa26113c6592c2cfb038b90f8b1bfffc5156d5
Casey Megginson <tenncasey@gmail.com>
2016-05-03 01:23:44 -0500

    Fixed D3 module import.

--- incarceration_rates/systemjs_setup.js
+++ incarceration_rates/systemjs_setup.js
@@ -4,11 +4,14 @@ System.config({
       format: 'register',
       defaultExtension: 'js'
     },
-    'node_modules/d3': {
+    '@d3/d3': {
       format: 'cjs',
       defaultExtension: 'js'
     },
   },
+  map: {
+      'd3': 'node_modules/d3'
+  }
 });
 System.import('executions.js')
       .then(null, console.error.bind(console));
